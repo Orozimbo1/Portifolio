354e4bf5fe1b238ec18a0d33b4a93ad757b82b3d
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-01-14 16:45:41 -0300

    criando o esboço do portfolio

--- react-portfolio/pages/index.js
+++ react-portfolio/pages/index.js
@@ -1,4 +1,7 @@
 import Head from 'next/head'
+import { BsFillMoonStarsFill } from 'react-icons/bs'
+import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -9,7 +12,129 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main className=' bg-white px-10 md:px-20'>
+        <section className=" min-h-screen">
+            <nav className='py-10 mb-12 flex justify-between'>
+              <h1 className='text-xl font-burtons'>developedbyed</h1>
+              <ul className='flex items-center'>
+                <li>
+                  <BsFillMoonStarsFill className='cursor-pointer text-2xl'/>
+                </li>
+                <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a></li>
+              </ul>
+            </nav>
+            <div className='text-center p-10'>
+              <h2 className='text-2xl py-2 text-teal-600 font-medium md:text-5xl'>Matheus Orozimbo</h2>
+              <h3 className='text-1xl py-2 md:text-3xl'>Desenvolvedor Full Stack</h3>
+              <p className='text-md py-5 leading-7 text-gray-500'>
+                Algumacoisasobremimalgum acoisasobremimalguma  coisa sobremimalgumacoisasobr emimalgumacois asobremimalgumacoisaso bremimalgumacoisasobremim
+              </p>
+            </div>
+            <div className='text-4xl flex justify-center gap-10 py-3 text-gray-600'>
+              <AiFillLinkedin />
+              <AiFillGithub />
+            </div>
+            <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 my-10 overflow-hidden'>
+              {/* <Image src={dev} layout="fill" objectFit="cover" /> */}
+            </div>
+        </section>
+        <section>
+          <div>
+            <h3 className='text-2xl py-1 md:text-3xl'>Serviços que presto</h3>
+            <p className='text-md leading-7 py-2'>
+              Um texto descrevendo o que eu sei fazer
+              Um texto descrevendo o que eu sei fazer
+              Um texto descrevendo o que eu sei fazer
+              Um texto descrevendo o que eu sei fazer
+              Um texto descrevendo o que eu sei fazer
+              Um texto descrevendo o que eu sei fazer
+            </p>
+            <div>
+              <div className='text-center shadow-lg rounded-xl p-10'>
+                {/* <Image src={imagemdoqueeufaço} layout="fill" objectFit="cover" /> */}
+                <div className='mx-auto h-40 w-40 bg-slate-700 rounded-xl my-10'></div>
+                <h3 className='text-lg font-medium pt-8 pb-2'>
+                  Titulo de uma area que faço
+                </h3>
+                <p className='text-md leading-7 py-2'>
+                  Um texto descrevendo essa área
+                </p>
+                <h4 className='py-4 text-teal-600'>
+                  Ferramentas que uso nessa área
+                </h4>
+                <p className='text-gray-800 py-1'>ferramenta</p>
+                <p className='text-gray-800 py-1'>ferramen</p>
+                <p className='text-gray-800 py-1'>ferra</p>
+                <p className='text-gray-800 py-1'>fe</p>
+                <p className='text-gray-800 py-1'>f</p>
+              </div>
+              <div className='text-center shadow-lg p-10 rounded-xl'>
+                {/* <Image src={imagemdoqueeufaço} layout="fill" objectFit="cover" /> */}
+                <div className='mx-auto h-40 w-40 bg-slate-700 rounded-xl my-10'></div>
+                <h3 className='text-lg font-medium pt-8 pb-2'>
+                  Titulo de uma area que faço
+                </h3>
+                <p className='text-md leading-7 py-2'>
+                  Um texto descrevendo essa área
+                </p>
+                <h4 className='py-4 text-teal-600'>
+                  Ferramentas que uso nessa área
+                </h4>
+                <p className='text-gray-800 py-1'>ferramenta</p>
+                <p className='text-gray-800 py-1'>ferramen</p>
+                <p className='text-gray-800 py-1'>ferra</p>
+                <p className='text-gray-800 py-1'>fe</p>
+                <p className='text-gray-800 py-1'>f</p>
+              </div>
+              <div className='text-center shadow-lg p-10 rounded-xl'>
+                {/* <Image src={imagemdoqueeufaço}/> */}
+                <div className='mx-auto h-40 w-40 bg-slate-700 rounded-xl my-10'></div>
+                <h3 className='text-lg font-medium pt-8 pb-2'>
+                  Titulo de uma area que faço
+                </h3>
+                <p className='text-md leading-7 py-2'>
+                  Um texto descrevendo essa área
+                </p>
+                <h4 className='py-4 text-teal-600'>
+                  Ferramentas que uso nessa área
+                </h4>
+                <p className='text-gray-800 py-1'>ferramenta</p>
+                <p className='text-gray-800 py-1'>ferramen</p>
+                <p className='text-gray-800 py-1'>ferra</p>
+                <p className='text-gray-800 py-1'>fe</p>
+                <p className='text-gray-800 py-1'>f</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className='text-2xl py-1 md:text-3xl'>Portfolio</h3>
+            <p className='text-md leading-7 py-2'>
+              Um texto descrevendo o que e meu portfolio
+              Um texto descrevendo o que e meu portfolio
+              Um texto descrevendo o que e meu portfolio
+              Um texto descrevendo o que e meu portfolio
+              Um texto descrevendo o que e meu portfolio
+              Um texto descrevendo o que e meu portfolio
+            </p>
+          </div>
+          <div>
+            <div>
+              {/* <Image src={Imagemdosmeuprojetos}/> */}
+              <div className='mx-auto h-80 w-100 bg-slate-700 rounded-xl my-10 md:min-h-screen'></div>
+            </div>
+            <div>
+              {/* <Image src={Imagemdosmeuprojetos}/> */}
+              <div className='mx-auto h-80 w-100 bg-slate-700 rounded-xl my-10 md:min-h-screen'></div>
+            </div>
+            <div>
+              {/* <Image src={Imagemdosmeuprojetos}/> */}
+              <div className='mx-auto h-80 w-100 bg-slate-700 rounded-xl my-10 md:min-h-screen'></div>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
