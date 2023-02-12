cb0c261c9ecbfa7637b6b39006f2f8f5d56a9d9c
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-02-12 10:58:56 -0300

    layout Navbar pronto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 
 const Navbar = () => {
+  const menuLinks = [
+    { name: "HOME", link: "#home" },
+    { name: "ABOUT", link: "#about" },
+    { name: "SKILLS", link: "#skills" },
+    { name: "PROJECTS", link: "#projects" },
+    { name: "CONTACTS", link: "#contacts" },
+  ]
+
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className='flex justify-between'>
+        <div className='mx-7'>
+          <h4 className='text-4xl uppercase font-bold'>
+            Ma<span className='text-cyan-600'>the</span>us
+          </h4>
+        </div>
+        <div className='text-gray-900 md:block hidden px-7 py-2 font-medium bg-white rounded-bl-full'>
+          <ul className='flex items-center gap-1 py-2 text-lg'>
+            {
+              menuLinks?.map((menu, i) => (
+                <li key={i} className='px-6 hover:text-cyan-600'>
+                  <a href={menu?.link}>{menu?.name}</a>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
