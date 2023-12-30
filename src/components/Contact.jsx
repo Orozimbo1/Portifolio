94fb2ed31ad659e11d1fa556cfb146210f5ec6c3
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-12-30 00:19:16 -0300

    Finalizado estilização

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,17 +10,17 @@ const Contact = () => {
   ]
 
   return (
-    <section id='contacts' className='py-10 px-3 text-white'>
+    <section id='contacts' className='py-10 px-5 text-teal-700'>
       <div className='text-center mt-8'>
-        <h3 className='text-4xl font-semibold'>
-          Entre em <span className='text-cyan-600'>Contato</span>
+        <h3 className='text-4xl font-semibold text-green-500'>
+          Entre em <span className='text-teal-700'>Contato</span>
         </h3>
-        <div className='mt-16 max-w-6xl bg-gray-800 lg:p-6 p-2 rounded-lg mx-auto'>
-          <div className='flex justify-around gap-6'>
+        <div className='mt-16 max-w-6xl lg:p-6 p-2 mx-auto'>
+          <div className='flex flex-col lg:flex-row justify-around gap-6'>
             {
               contact_info?.map((contact, i) => (
                 <div key={i} className="flex gap-4 w-fit items-center">
-                  <div className='min-w-[3.5rem] lg:text-3xl text-2xl min-h-[3.5rem] flex items-center justify-center text-white bg-cyan-600 rounded-full'>
+                  <div className='min-w-[3.5rem] lg:text-3xl text-2xl min-h-[3.5rem] flex items-center justify-center text-white bg-green-500 rounded-full'>
                     {  
                       <a href={contact.link} className='flex items-center justify-center'>
                         <ion-icon name={contact.logo}></ion-icon>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -29,21 +29,30 @@ const Project = () => {
   ]
 
   return (
-    <section id='projects' className='py-10 text-green-500'>
+    <section id='projects' className='py-10 text-teal-700'>
       <div className='text-center mb-4'>
-        <h3 className='text-4xl font-semibold'>
+        <h3 className='text-4xl font-semibold text-green-500'>
           Meus <span className='text-teal-700'>Projetos</span>
         </h3>
         <p className='text-teal-700 mt-3 text-lg'>Meus incríveis trabalhos</p>
       </div>
-      <div className='lg:w-11/12 w-full mx-auto'>
+      <div className='lg:w-11/12 w-10/12 mx-auto'>
         <Swiper 
           slidesPerView={3} 
           spaceBetween={20} 
           breakpoints={{
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 40
+            },
             768: {
               slidesPerView: 3,
-            }
+              spaceBetween: 40
+            },
+            1440: {
+              slidesPerView: 4,
+              spaceBetween: 40
+            },
           }} 
           loop={true} 
           autoplay={{
@@ -61,7 +70,7 @@ const Project = () => {
                 <h3 className='text-xl my-4'>{project.name}</h3>
                 <div className='flex gap-3'>
                   <a href={project.link_github} className='text-white bg-green-500 p-2 inline-block' target='_blank'>Link Github</a>
-                  <a href={project.live_demo} className='text-teal-700 bg-gray-100 border border-teal-700 p-2 inline-block' target='_blank'>Live Demo</a>
+                  <a href={project.live_demo} className='text-teal-700 bg-gray-100 border border-teal-700 p-2 inline-block rounded-md' target='_blank'>Live Demo</a>
                 </div>
               </div>
             </SwiperSlide>
